337d05ba684a3ae6a4076f7ed298e5e5609fa535
Tahir Fazal <tahirawan4@gmail.com>
2018-04-26 10:03:38 +0500

    adding py file

---
+++ first_program.py
@@ -0,0 +1,30 @@
+print('Hello World')
+
+number = 15
+number2 ='This is another string with number 17.'
+
+print(number)
+print(number2)
+
+
+def my_function():
+    print('this is my first function')
+    if number == 14:
+        print('It is true')
+
+
+
+list_name = ['One','Two', 'Three']
+list_roll_number = ['1','2','3']
+
+for index in range(3):
+    print(list_name[index])
+    print(list_roll_number[index])
+
+
+
+student_dict = {'1':['Name','3.19'],'2':['Name','3.19'],'3':['Name','3.19']}
+
+print(student_dict.get('3'))
+
+my_function()
